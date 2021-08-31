d7d8c332d82486d877bbf95d33d5363327d4559a
T. Khoa Phan <ptkhoa1984@users.noreply.github.com>
2021-08-31 21:14:28 +0100

    Update test_kubenetesPodOperator.py

--- dags/test_kubenetesPodOperator.py
+++ dags/test_kubenetesPodOperator.py
@@ -1,51 +1,103 @@
-from datetime import timedelta
-
 from airflow import DAG
+from datetime import datetime, timedelta
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
 from airflow.operators.dummy_operator import DummyOperator
+
+from textwrap import dedent
+
+# Operators; we need this to operate!
+from airflow.operators.bash import BashOperator
+from airflow.contrib.sensors.file_sensor import FileSensor
 from airflow.utils.dates import days_ago
 
+
 default_args = {
-    'owner': 'Airflow',
+    'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': days_ago(0),
-    'catchup': False,
+    'start_date': datetime.utcnow(),
     'email': ['airflow@example.com'],
     'email_on_failure': False,
     'email_on_retry': False,
-    'retries': 0,
-    'retry_delay': timedelta(minutes=5),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1)
 }
 
-dag = DAG(
-    'dag_that_executes_via_KubernetesPodOperator',
+
+with DAG(
+    'kubernetes_sample',
     default_args=default_args,
-    schedule_interval=timedelta(minutes=30),
-    max_active_runs=1,
-    concurrency=10
-)
+    description='kubernetes_sample',
+    schedule_interval=timedelta(days=1),
+    start_date=days_ago(1),
+    tags=['kubernetes_sample'],
+    catchup=False # the scheduler creates a DAG run only for the latest interval
+) as dag:
+    # [END instantiate_dag]
 
-# Generate 2 tasks
-tasks = ["task{}".format(i) for i in range(1, 3)]
-example_dag_complete_node = DummyOperator(task_id="example_dag_complete", dag=dag)
-
-org_dags = []
-for task in tasks:
-
-    bash_command = 'echo HELLO'
-
-    org_node = KubernetesPodOperator(
-        namespace='default',
-        image="python",
-        cmds=["python", "-c"],
-        arguments=["print('HELLO')"],
-        labels={"foo": "bar"},
-        image_pull_policy="Always",
-        name=task,
-        task_id=task,
-        is_delete_operator_pod=False,
-        get_logs=True,
-        dag=dag
+    t0 = BashOperator(
+        task_id='Forecast',
+        depends_on_past=False,
+        bash_command="echo this is Forecast; sleep 1"
     )
 
-    org_node.set_downstream(example_dag_complete_node)
+    t1 = BashOperator(
+        task_id='trans_1',
+        depends_on_past=False,
+        bash_command="echo this is trans_1; sleep 1"
+    )
+
+    t2 = KubernetesPodOperator(
+        namespace='default',
+        image="python",
+        cmds=["bash", "-cx"],
+        arguments=["echo", "Khoa 10"],
+        name="ubuntu_18_0_4_1",
+        task_id="Fluenta_from_GCR",
+        get_logs=True,
+    )
+
+    t3 = BashOperator(
+        task_id='trans_2',
+        depends_on_past=False,
+        bash_command="echo this is trans_2; sleep 1"
+    )
+
+    # t4 = KubernetesPodOperator(
+    #     namespace='default',
+    #     image='gcr.io/gcp-runtimes/ubuntu_18_0_4',
+    #     cmds=["sh", "-c", "echo 'Hello, this is Dorset from GCR...'"],
+    #     name="ubuntu_18_0_4_1",
+    #     task_id="Dorset_from_GCR",
+    #     get_logs=True
+    # )
+
+    # t5 = BashOperator(
+    #     task_id='Planning_and_Budgeting',
+    #     bash_command="echo Planning_and_Budgeting"
+    # )    
+    # t0 >> t1 >> t2 >> t3 >> t4 >> t5
+    t0 >> t1 >> t2 >> t3
+
+
+    # start = DummyOperator(task_id='run_this_first')
+
+    # passing = KubernetesPodOperator(namespace='default',
+    #                       image='gcr.io/gcp-runtimes/ubuntu_18_0_4',
+    #                       cmds=["Python","-c"],
+    #                       arguments=["print('hello world')"],
+    #                       labels={"foo": "bar"},
+    #                       name="ubuntu_18_0_4_1",
+    #                       task_id="ubuntu_18_0_4_1",
+    #                       get_logs=True
+    #                       )
+
+    # failing = KubernetesPodOperator(namespace='default',
+    #                       image='gcr.io/gcp-runtimes/ubuntu_18_0_4',
+    #                       cmds=["sh", "-c", 'echo \'Sleeping..\'; echo \'Done!\''],
+    #                       labels={"foo": "bar"},
+    #                       name="ubuntu_18_0_4_2",
+    #                       task_id="ubuntu_18_0_4_2",
+    #                       get_logs=True
+    #                       )
+  
+    # start >> [passing, failing]
