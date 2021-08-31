7139b3e5a3e2fcd407cb01770c042a468b0e0577
T. Khoa Phan <ptkhoa1984@users.noreply.github.com>
2021-08-31 16:28:12 +0100

    Create dummy_dag.py

---
+++ dags/dummy_dag.py
@@ -0,0 +1,94 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+"""
+### Tutorial Documentation
+Documentation that goes along with the Airflow tutorial located
+[here](https://airflow.apache.org/tutorial.html)
+"""
+# [START tutorial]
+# [START import_module]
+from datetime import timedelta
+from textwrap import dedent
+
+# The DAG object; we'll need this to instantiate a DAG
+from airflow import DAG
+
+# Operators; we need this to operate!
+from airflow.operators.bash import BashOperator
+from airflow.contrib.sensors.file_sensor import FileSensor
+from airflow.utils.dates import days_ago
+
+# [END import_module]
+
+# [START default_args]
+# These args will get passed on to each operator
+# You can override them on a per-task basis during operator initialization
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'schedule_interval': '@daily',
+    # 'retries': 1,
+    # 'retry_delay': timedelta(minutes=5),
+    # 'queue': 'bash_queue',
+    # 'pool': 'backfill',
+    # 'priority_weight': 10,
+    # 'end_date': datetime(2016, 1, 1),
+    # 'wait_for_downstream': False,
+    # 'dag': dag,
+    # 'sla': timedelta(hours=2),
+    # 'execution_timeout': timedelta(seconds=300),
+    # 'on_failure_callback': some_function,
+    # 'on_success_callback': some_other_function,
+    # 'on_retry_callback': another_function,
+    # 'sla_miss_callback': yet_another_function,
+    # 'trigger_rule': 'all_success'
+}
+# [END default_args]
+
+# [START instantiate_dag]
+with DAG(
+    'pop_blueprint',
+    default_args=default_args,
+    description='PoP blueprint DAG',
+    schedule_interval=timedelta(days=1),
+    start_date=days_ago(1),
+    tags=['pop_blueprint'],
+    catchup=False
+) as dag:
+    # [END instantiate_dag]
+
+    t0 = BashOperator(
+        task_id='task t0',
+        depends_on_past=False,
+        bash_command='echo sleeping; sleep 5',
+    )
+
+    t1 = BashOperator(
+        task_id='task t1',
+        depends_on_past=False,
+        bash_command='echo sleeping; sleep 5',
+    )
+    t0 >> t1
+    # t0 >> [t1, t2] >> t3 >> t4
+
+
+# [END tutorial]
