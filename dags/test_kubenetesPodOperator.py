55f9b20b448984771e42ab0b82a867859e7bcc63
T. Khoa Phan <ptkhoa1984@users.noreply.github.com>
2021-09-09 17:14:52 +0100

    Update test_kubenetesPodOperator.py

--- dags/test_kubenetesPodOperator.py
+++ dags/test_kubenetesPodOperator.py
@@ -51,7 +51,7 @@ with DAG(
         namespace='airflow',
         image='eu.gcr.io/skyuk-uk-dsas-poc/alpine-linux:latest',
         cmds=["sh", "-c", "echo 'Hello, this is Fluenta from GCR...'"],
-        image_pull_secrets=[k8s.V1LocalObjectReference('testgcr1')],
+        image_pull_secrets=[k8s.V1LocalObjectReference('testgcrxxx')],
         name="fluenta-docker",
         task_id="Fluenta_from_GCR",
         get_logs=True
