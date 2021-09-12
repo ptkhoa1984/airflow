26281930be42d6dfe20c2c53f350048aa489da10
T. Khoa Phan <ptkhoa1984@users.noreply.github.com>
2021-09-12 15:22:36 +0100

    Update test_kubenetesPodOperator.py

--- dags/test_kubenetesPodOperator.py
+++ dags/test_kubenetesPodOperator.py
@@ -1,103 +1,165 @@
-from airflow import DAG
-from datetime import datetime, timedelta
-from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
-from airflow.operators.dummy_operator import DummyOperator
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
 
+"""
+### Tutorial Documentation
+Documentation that goes along with the Airflow tutorial located
+[here](https://airflow.apache.org/tutorial.html)
+"""
+# [START tutorial]
+# [START import_module]
+from datetime import timedelta
 from textwrap import dedent
-from kubernetes.client import models as k8s
+
+# The DAG object; we'll need this to instantiate a DAG
+from airflow import DAG
 
 # Operators; we need this to operate!
 from airflow.operators.bash import BashOperator
 from airflow.contrib.sensors.file_sensor import FileSensor
+from airflow.sensors.sql import SqlSensor
 from airflow.utils.dates import days_ago
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
 
+from textwrap import dedent
+from kubernetes.client import models as k8s
 
+# [END import_module]
+
+############################### common variables ###############################
+FORECAST_ID = 2417
+CDN_PLAN_ID = 2359
+TERRITORY = "UK"
+FAILOVER_ID = 33
+ID = 343
+T0_COMMAND = f"select * from inventory.end2end_path where id = {ID}"
+PERIOD = "'Y20.06'"
+
+####################### variables when running on premise ######################
+BASE_PATH = "/Users/tpp02/OneDrive\ -\ Sky/code"
+
+T1_COMMAND = f"/opt/data/core_model/core_model/planning/Fluenta/fluenta_plan_w_sql.py \
+        --forecast_id={FORECAST_ID} \
+        --cdn_plan_id={CDN_PLAN_ID} \
+        --territory={TERRITORY} \
+        --failover_id={FAILOVER_ID} \
+        --filename /opt/data/core_model/core_model/planning/Fluenta/khoa --soip"
+
+T2_COMMAND = f"python /opt/data/fluenta-master/fluenta/app.py \
+        -path_in /opt/data/fluenta-master/tests/test_data/test_data_plan_p350.txt \
+        -filename /opt/data/fluenta-master/fluenta/out \
+        -period {PERIOD}"
+
+T3_COMMAND = f"python {BASE_PATH}/khoa_code/airflow_pip/transform_fluenta_output.py"
+
+T4_COMMAND = f"python /opt/data/dorset-develop/poc/pop_tactical/main_pop_tactical.py"
+
+# [START default_args]
+# These args will get passed on to each operator
+# You can override them on a per-task basis during operator initialization
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime.utcnow(),
     'email': ['airflow@example.com'],
     'email_on_failure': False,
     'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=1)
+    'schedule_interval': '@daily',    
 }
+# [END default_args]
 
-
+# [START instantiate_dag]
 with DAG(
-    'kubernetes_sample',
+    'SC_pipeline_half_real',
     default_args=default_args,
-    description='kubernetes_sample',
+    description='SC_pipeline_half_real',
     schedule_interval=timedelta(days=1),
     start_date=days_ago(1),
-    tags=['kubernetes_sample'],
-    catchup=False # the scheduler creates a DAG run only for the latest interval
+    tags=['SC_pipeline_half_real'],
+    catchup=False
 ) as dag:
     # [END instantiate_dag]
 
-    t0 = BashOperator(
+
+    # add username/pass/url in postgres_default (in Admin/Connections/postgres_default)
+    t0 = SqlSensor(
         task_id='Forecast',
-        depends_on_past=False,
-        bash_command="echo this is Forecast; sleep 1"
+        conn_id='postgres_default',
+        sql=T0_COMMAND
     )
 
-    t1 = BashOperator(
-        task_id='trans_1',
-        depends_on_past=False,
-        bash_command="echo this is trans_1; sleep 1"
+    t1 = KubernetesPodOperator(
+        namespace='airflow',
+        image='eu.gcr.io/skyuk-uk-dsas-poc/kp-core-model-ubuntu:0.1',
+        cmds=["sh", "-c", T1_COMMAND],
+        name="trans_1",
+        task_id="trans_1",
+        get_logs=True
     )
+
+
+    # t1 = BashOperator(
+    #     task_id='trans_1',
+    #     depends_on_past=False,
+    #     bash_command=T1_COMMAND
+    # )
+
+    # t2 = BashOperator(
+    #     task_id='Fluenta',
+    #     depends_on_past=False,
+    #     bash_command=T2_COMMAND
+    # )
 
     t2 = KubernetesPodOperator(
         namespace='airflow',
-        image='eu.gcr.io/skyuk-uk-dsas-poc/alpine-linux:latest',
-        cmds=["sh", "-c", "echo 'Hello, this is Fluenta from GCR...'"],
-#         image_pull_secrets='testgcr',
-        name="fluenta-docker",
-        task_id="Fluenta_from_GCR",
+        image='eu.gcr.io/skyuk-uk-dsas-poc/kp-fluenta-ubuntu:0.1',
+        cmds=["sh", "-c", T2_COMMAND],
+        name="Fluenta",
+        task_id="Fluenta",
         get_logs=True
     )
+
 
     t3 = BashOperator(
         task_id='trans_2',
         depends_on_past=False,
-        bash_command="echo this is trans_2; sleep 1"
+        # bash_command=T3_COMMAND,
+        bash_command="echo 'This is trans_2'"
     )
+
+    # t4 = BashOperator(
+    #     task_id='Dorset',
+    #     bash_command=T4_COMMAND
+    # )
 
     t4 = KubernetesPodOperator(
         namespace='airflow',
-        image='gcr.io/gcp-runtimes/ubuntu_18_0_4',
-        cmds=["sh", "-c", "echo 'Hello, this is Dorset from GCR...'"],
-        name="ubuntu_18_0_4_2",
-        task_id="Dorset_from_GCR",
+        image='eu.gcr.io/skyuk-uk-dsas-poc/kp-dorset-ubuntu:0.1',
+        cmds=["sh", "-c", T4_COMMAND],
+        name="Dorset",
+        task_id="Dorset",
         get_logs=True
-    )
+    ) 
 
     t5 = BashOperator(
         task_id='Planning_and_Budgeting',
-        bash_command="echo Planning_and_Budgeting"
+        bash_command="echo 'This is Planning_and_Budgeting'"
     )    
     t0 >> t1 >> t2 >> t3 >> t4 >> t5
 
 
-    # start = DummyOperator(task_id='run_this_first')
-
-    # passing = KubernetesPodOperator(namespace='default',
-    #                       image='gcr.io/gcp-runtimes/ubuntu_18_0_4',
-    #                       cmds=["Python","-c"],
-    #                       arguments=["print('hello world')"],
-    #                       labels={"foo": "bar"},
-    #                       name="ubuntu_18_0_4_1",
-    #                       task_id="ubuntu_18_0_4_1",
-    #                       get_logs=True
-    #                       )
-
-    # failing = KubernetesPodOperator(namespace='default',
-    #                       image='gcr.io/gcp-runtimes/ubuntu_18_0_4',
-    #                       cmds=["sh", "-c", 'echo \'Sleeping..\'; echo \'Done!\''],
-    #                       labels={"foo": "bar"},
-    #                       name="ubuntu_18_0_4_2",
-    #                       task_id="ubuntu_18_0_4_2",
-    #                       get_logs=True
-    #                       )
-  
-    # start >> [passing, failing]
+# [END tutorial]
