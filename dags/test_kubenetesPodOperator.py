ad080ccac0063f1152407a3314fc0a3aef09e719
T. Khoa Phan <ptkhoa1984@users.noreply.github.com>
2021-09-13 10:53:27 +0100

    Update test_kubenetesPodOperator.py

--- dags/test_kubenetesPodOperator.py
+++ dags/test_kubenetesPodOperator.py
@@ -1,28 +1,39 @@
-from kubernetes.client import V1Volume, V1SecretVolumeSource, V1VolumeMount
+from kubernetes.client import V1VolumeMount, V1Volume, V1PersistentVolumeClaimVolumeSource
 
 from airflow import DAG
-from airflow.kubernetes.secret import Secret
 from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
 from airflow.utils.dates import days_ago
 
-with DAG(dag_id="example_k8s_secret_volume", start_date=days_ago(1), schedule_interval='@once', tags=["example"]) as dag:
+with DAG(dag_id="example_k8s_volume", start_date=days_ago(1),
+         schedule_interval='@once', tags=["example"]) as dag:
+    myapp_volume = V1Volume(
+        name='myapp-volume',
+        persistent_volume_claim=V1PersistentVolumeClaimVolumeSource(claim_name='myapp-pvc-rw'))
 
-    secret = Secret('volume', '/etc/my-secret', 'db-credentials')
-    # Is Equal to below two lines
-    # secret_volume = V1Volume(name='my-secret-vol', secret=V1SecretVolumeSource(secret_name='db-credentials'))
-    # secret_volume_mount = V1VolumeMount(mount_path='/etc/my-secret', name='my-secret-vol', read_only=True)
+    myapp_volume_mount = V1VolumeMount(mount_path='/root/myapp', name='myapp-volume')
 
-    task1 = KubernetesPodOperator(task_id='SC_pipeline_half_real',
-                                  name='SC_pipeline_half_real',
-                                  namespace='airflow',
-                                  secrets=[secret, ],
-                                  # secrets is equal to below two lines
-                                  # volumes=[secret_volume, ],
-                                  # volume_mounts=[secret_volume_mount, ],
+    task1 = KubernetesPodOperator(task_id='k8s_volume_read_task',
+                                  name='airflow_pod_volume_read',
+                                  namespace='default',
                                   image='alpine',
+                                  volumes=[myapp_volume, ],
+                                  volume_mounts=[myapp_volume_mount, ],
                                   cmds=["sh", "-c",
-                                        'echo "Secret Directory Content "$(ls -l /etc/my-secret)'],
-                                  in_cluster=False,
+                                        'date > /root/myapp/date.txt',
+                                        ],
                                   startup_timeout_seconds=60,
                                   )
-    task1
+
+    task2 = KubernetesPodOperator(task_id='k8s_volume_write_task',
+                                  name='airflow_pod_volume_write',
+                                  namespace='default',
+                                  image='alpine',
+                                  volumes=[myapp_volume, ],
+                                  volume_mounts=[myapp_volume_mount, ],
+                                  cmds=["sh", "-c",
+                                        'echo "Reading date from date.txt : "$(cat /root/myapp/date.txt)',
+                                        ],
+                                  startup_timeout_seconds=60,
+                                  )
+
+    task1 >> task2
